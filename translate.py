2cf1241580962893dd7b3b3cd7542080ec5f9d55
aosakwe <adrien.osakwe@mail.mcgill.ca>
2021-02-19 19:44:11 -0500

    Fixed error in dictionary (missing comma)

--- translate.py
+++ translate.py
@@ -1,8 +1,8 @@
 #Solution to RNA to Protein problem
 #Code is set up such that we check each ORF to find a START codon before translating the sequence
-codon = {"ATT": 'I' ,"ATC": 'I', "ATA" : 'I',"CTT": 'L' , "CTC" : 'L', "CTA": 'L', "CTG": 'L', "TTA": 'L', "TTG": 'L',"GTT":'V', "GTC":'V', "GTA":'V', "GTG":'V',"TTT":'F',"TTC":'F',"ATG":'M',"TGT":'C',"TGC":'C',"GCT":'A',"GCC":'A', "GCA":'A', "GCG":'A',"GGT":'G',"GGC" :'G',"GGA" :'G',"GGG" :'G',"CCT":'P',"CCC":'P',"CCA":'P',"CCG":'P',"ACT":'T',"ACC":'T',"ACA":'T',"ACG":'T',"TCT":'S',"TCC":'S',"TCA":'S',"TCG":'S',"AGT":'S',"AGC":'S',"TAT":'Y',"TAC":'Y',"TGG":'W',"CAA":'Q',"CAG":'Q',"AAT":'N',"AAC":'N',"CAT":'H',"CAC":'H',"GAA":'E',"GAG":'E',"GAT":'D',"GAC":'D',"AAA":'K',"AAG":'K',"CGT":'R',"CGC":'R',"CGA":'R',"CGG":'R',"AGA":'R',"AGG":'R'"TAA":"STOP","TAG":"STOP","TGA":"STOP"}
+codon = {"ATT":'I' ,"ATC":'I', "ATA":'I', "CTT":'L' , "CTC" : 'L', "CTA": 'L', "CTG": 'L', "TTA": 'L', "TTG": 'L',"GTT":'V', "GTC":'V', "GTA":'V', "GTG":'V',"TTT":'F',"TTC":'F',"ATG":'M',"TGT":'C',"TGC":'C',"GCT":'A',"GCC":'A', "GCA":'A', "GCG":'A',"GGT":'G',"GGC" :'G',"GGA" :'G',"GGG" :'G',"CCT":'P',"CCC":'P',"CCA":'P',"CCG":'P',"ACT":'T',"ACC":'T',"ACA":'T',"ACG":'T',"TCT":'S',"TCC":'S',"TCA":'S',"TCG":'S',"AGT":'S',"AGC":'S',"TAT":'Y',"TAC":'Y',"TGG":'W',"CAA":'Q',"CAG":'Q',"AAT":'N',"AAC":'N',"CAT":'H',"CAC":'H',"GAA":'E',"GAG":'E',"GAT":'D',"GAC":'D',"AAA":'K',"AAG":'K',"CGT":'R',"CGC":'R',"CGA":'R',"CGG":'R',"AGA":'R',"AGG":'R',"TAA":"STOP","TAG":"STOP","TGA":"STOP"}
 protein = []
-seq = Input("Enter RNA Sequence")
+seq = input("Enter RNA Sequence")
 i = 0
 while seq[i:i+3] != "AUG":
 	i += 1
