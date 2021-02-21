8bc00c882c906d13dad99ae7be90d7dbf8b9ce0b
aosakwe <adrien.osakwe@mail.mcgill.ca>
2021-02-21 15:13:21 -0500

    Completed script to show protein and translated reading frame.

--- translate.py
+++ translate.py
@@ -1,11 +1,12 @@
 #Solution to RNA to Protein problem
 #Code is set up such that we check each ORF to find a START codon before translating the sequence
-codon = {"ATT":'I' ,"ATC":'I', "ATA":'I', "CTT":'L' , "CTC" : 'L', "CTA": 'L', "CTG": 'L', "TTA": 'L', "TTG": 'L',"GTT":'V', "GTC":'V', "GTA":'V', "GTG":'V',"TTT":'F',"TTC":'F',"ATG":'M',"TGT":'C',"TGC":'C',"GCT":'A',"GCC":'A', "GCA":'A', "GCG":'A',"GGT":'G',"GGC" :'G',"GGA" :'G',"GGG" :'G',"CCT":'P',"CCC":'P',"CCA":'P',"CCG":'P',"ACT":'T',"ACC":'T',"ACA":'T',"ACG":'T',"TCT":'S',"TCC":'S',"TCA":'S',"TCG":'S',"AGT":'S',"AGC":'S',"TAT":'Y',"TAC":'Y',"TGG":'W',"CAA":'Q',"CAG":'Q',"AAT":'N',"AAC":'N',"CAT":'H',"CAC":'H',"GAA":'E',"GAG":'E',"GAT":'D',"GAC":'D',"AAA":'K',"AAG":'K',"CGT":'R',"CGC":'R',"CGA":'R',"CGG":'R',"AGA":'R',"AGG":'R',"TAA":"STOP","TAG":"STOP","TGA":"STOP"}
+codon = {"AUU":'I' ,"AUC":'I', "AUA":'I', "CUU":'L' , "CUC" : 'L', "CUA": 'L', "CUG": 'L', "UUA": 'L', "UUG": 'L',"GUU":'V', "GUC":'V', "GUA":'V', "GUG":'V',"UUU":'F',"UUC":'F',"AUG":'M',"UGU":'C',"UGC":'C',"GCU":'A',"GCC":'A', "GCA":'A', "GCG":'A',"GGU":'G',"GGC" :'G',"GGA" :'G',"GGG" :'G',"CCU":'P',"CCC":'P',"CCA":'P',"CCG":'P',"ACU":'T',"ACC":'T',"ACA":'T',"ACG":'T',"UCU":'S',"UCC":'S',"UCA":'S',"UCG":'S',"AGU":'S',"AGC":'S',"UAU":'Y',"UAC":'Y',"UGG":'W',"CAA":'Q',"CAG":'Q',"AAU":'N',"AAC":'N',"CAT":'H',"CAC":'H',"GAA":'E',"GAG":'E',"GAU":'D',"GAC":'D',"AAA":'K',"AAG":'K',"CGU":'R',"CGC":'R',"CGA":'R',"CGG":'R',"AGA":'R',"AGG":'R',"UAA":"STOP","UAG":"STOP","UGA":"STOP"}
 protein = []
-seq = input("Enter RNA Sequence")
+seq = input("Enter RNA Sequence: ")
 i = 0
 while seq[i:i+3] != "AUG":
 	i += 1
+start = i
 protein.append(codon[seq[i:i+3]])
 i += 3
 while i+3 <= len(seq):
@@ -13,7 +14,10 @@ while i+3 <= len(seq):
 		raise ValueError("Codon does not exist")
 	elif seq[i:i+3] == "STOP":
 		break
-	protein.append(codon[seq[i:i+3]])
-	i += 3
-print("".join(protein))
+	else:
+		protein.append(codon[seq[i:i+3]])
+		i += 3
+end = i + 3
+print("Reading frame: " + seq[start:end])
+print("Protein sequence: " + "".join(protein))
 		
