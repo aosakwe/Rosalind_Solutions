069bee1e16575c8e195fa6e0efe6b9d3647f7d32
aosakwe <adrien.osakwe@mail.mcgill.ca>
2021-03-02 10:10:09 -0500

    Solution to mortal rabbits fibonacci problem

---
+++ mortal_rabbit.py
@@ -0,0 +1,16 @@
+#Code to Solve Mortal Rabbits problem
+#Need a way to keep track of rabbits alive three rounds ago
+n = int(input("Number of months: "))
+k = int(input("Number of pairs of rabbits produced by a pair: "))
+prev = 0 #previous month's population
+temp = 0 #temporary value
+cur = 1 # current month's population
+for i in range(n-1):
+	temp = cur + k*prev
+	prev = cur
+	cur = temp
+	print(cur)
+
+
+	
+
