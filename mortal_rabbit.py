ec011273a86dd73b6529f11b1841ced5b1f34841
aosakwe <adrien.osakwe@mail.mcgill.ca>
2021-03-02 10:28:42 -0500

    Need to improve tracing of m-old rabbit population

--- mortal_rabbit.py
+++ mortal_rabbit.py
@@ -1,15 +1,30 @@
+#!/bin/python3
 #Code to Solve Mortal Rabbits problem
-#Need a way to keep track of rabbits alive three rounds ago
+#Can trace number of rabbits that have lived for m months by running a second algorithm delayed by m months
+#doesn't work as the tracking of m old pop also needs to account for deaths
 n = int(input("Number of months: "))
-k = int(input("Number of pairs of rabbits produced by a pair: "))
+m = int(input("Lifespan of rabbits: "))
 prev = 0 #previous month's population
 temp = 0 #temporary value
 cur = 1 # current month's population
+prevd = 0 
+tempd = 0
+curd = 1
+counter = 0 
 for i in range(n-1):
-	temp = cur + k*prev
+	temp = cur + prev
 	prev = cur
 	cur = temp
-	print(cur)
+	counter += 1
+	if counter == m:
+		cur -= 1
+	elif counter > m:
+		tempd = curd + prevd
+		prevd = curd
+		curd = tempd
+		cur -= curd
+
+print(cur)
 
 
 	
