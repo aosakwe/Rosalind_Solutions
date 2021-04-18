087011c788e2b65cba873052c688613fec3503e1
aosakwe <adrien.osakwe@mail.mcgill.ca>
2021-04-17 21:54:15 -0400

    Alternative solutions to recursion problems

--- mortal_rabbit.py
+++ mortal_rabbit.py
@@ -1,31 +1,16 @@
 #!/bin/python3
 #Code to Solve Mortal Rabbits problem
-#Can trace number of rabbits that have lived for m months by running a second algorithm delayed by m months
-#doesn't work as the tracking of m old pop also needs to account for deaths
+
 n = int(input("Number of months: "))
 m = int(input("Lifespan of rabbits: "))
-prev = 0 #previous month's population
-temp = 0 #temporary value
-cur = 1 # current month's population
-prevd = 0 
-tempd = 0
-curd = 1
-counter = 0 
-for i in range(n-1):
-	temp = cur + prev
-	prev = cur
-	cur = temp
-	counter += 1
-	if counter == m:
-		cur -= 1
-	elif counter > m:
-		tempd = curd + prevd
-		prevd = curd
-		curd = tempd
-		cur -= curd
 
-print(cur)
-
-
+def mortalfib(n,m):
+	x = [1]*100
+	for i in range(2,n):
+		x[i] = x[i-1] + x[i-2]
+		if i >= m:
+			x[i] -= x[(i-m)-1]
+	return x[i]
+print(mortalfib(n,m))
 	
 
