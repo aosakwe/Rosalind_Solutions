012a18c35d97d6668d326779e9ed510378497bb4
aosakwe <adrien.osakwe@mail.mcgill.ca>
2022-02-12 11:41:27 -0500

    deleted extra files

--- transcribe.py
+++
@@ -1,9 +0,0 @@
-#code for transcribing input DNA sequence
-seq = list(input("Enter a DNA sequence to transcribe: "))
-
-for i in range(len(seq)):
-	if seq[i] != 'A'  and seq[i] != 'C' and seq[i] != 'G' and seq[i] != 'T':
-		raise ValueError("Invalid Nucleotide detected") 
-	if seq[i] == 'T':
-		seq[i] = 'U'
-print("".join(seq))
